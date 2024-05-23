8e7c241ae4dc5d84d9689beb949dafb763ad85fe
poojaagrawal1915 <https://github.com/poojaagrawal1915>
2024-05-23 13:05:46 +0530

    Commiting the files

--- template.py
+++ template.py
@@ -1,1 +1,29 @@
-"Adding log in template again"
+import os
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
+list_of_files=[
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
+    "setup.py",
+    "requirements.txt",
+    "app.py",
+    "research/trails.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath= Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the files: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open (filepath,"w") as f:
+            pass
+            logging.info(f"Creatinf empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
